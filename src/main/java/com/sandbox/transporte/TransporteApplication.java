f0faf9f512f94f95c122b2b2812aa0cc2d680af2
pablo.manquillo <paarma80@gmail.com>
2022-10-26 00:12:13 -0500

    Se agregan capas correspondientes a la arquitectura hexagonal

--- src/main/java/com/sandbox/transporte/TransporteApplication.java
+++ src/main/java/com/sandbox/transporte/TransporteApplication.java
@@ -2,7 +2,9 @@ package com.sandbox.transporte;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.sandbox")
 @SpringBootApplication
 public class TransporteApplication {
 
