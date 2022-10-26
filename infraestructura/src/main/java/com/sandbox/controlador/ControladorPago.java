90446e391103accb172a895e68dfc14fe86e2106
pablo.manquillo <paarma80@gmail.com>
2022-10-26 00:29:29 -0500

    Se configura archivo de propiedades

---
+++ infraestructura/src/main/java/com/sandbox/controlador/ControladorPago.java
@@ -0,0 +1,12 @@
+package com.sandbox.controlador;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/pagos")
+public class ControladorPago {
+
+}
