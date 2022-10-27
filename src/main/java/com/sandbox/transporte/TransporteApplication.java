9e85f137954109c5246c7b5fcc1ee9113e8fdca9
pablo.manquillo <pablo.manquillo@ceiba.com.co>
2022-10-26 23:38:57 -0500

    Se agrega libreria jasypt para criptografia

--- src/main/java/com/sandbox/transporte/TransporteApplication.java
+++ src/main/java/com/sandbox/transporte/TransporteApplication.java
@@ -1,9 +1,11 @@
 package com.sandbox.transporte;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableEncryptableProperties
 @ComponentScan("com.sandbox")
 @SpringBootApplication
 public class TransporteApplication {
